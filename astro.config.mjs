baa8adafc4166151dcfd369342c990c1ed1cd34e
John Mikhail <johnfwilliam@gmail.com>
2026-03-29 10:01:35 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+    site: 'https://recordio.cc',
+    trailingSlash: 'always',
     integrations: [
         react(),
+        sitemap({
+            filter: (page) =>
+                !page.includes('/auth/') &&
+                !page.includes('/subscription-success') &&
+                !page.includes('/404') &&
+                !page.includes('/privacy') &&
+                !page.includes('/terms'),
+        }),
     ],
     output: 'static',
 });
